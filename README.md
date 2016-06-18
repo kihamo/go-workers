68dc5ca77d5c4cdf78cffd524244c46720c0735e
Sergey Fedosenko <sergey.fedosenko@lazada.com>
2016-06-19 01:15:35 +0300

    Add Build Status and Coverage Status and GoDoc

---
+++ README.md
@@ -0,0 +1,6 @@
+Go workers
+================
+
+[![Build Status](https://travis-ci.org/kihamo/go-workers.svg?branch=master)](https://travis-ci.org/kihamo/go-workers)
+[![Coverage Status](https://coveralls.io/repos/github/kihamo/go-workers/badge.svg?branch=master)](https://coveralls.io/github/kihamo/go-workers?branch=master)
+[![GoDoc](https://godoc.org/github.com/kihamo/go-workers?status.svg)](https://godoc.org/github.com/kihamo/go-workers)
--- worker_test.go
+++ worker_test.go
@@ -3,20 +3,30 @@ package workers
 import (
 	"testing"
 	"time"
+
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
+
+type WorkerSuite struct {
+	suite.Suite
+}
 
 func jobSimple(attempts int64, quit chan bool, args ...interface{}) (int64, time.Duration) {
 	return 1, time.Second
 }
 
 func Test_WorkerNewInstance_StatusIsWait(t *testing.T) {
+	suite.Run(t, new(WorkerSuite))
+
 	worker := NewWorker()
 
 	assert.Equal(t, worker.GetStatus(), WorkerStatusWait)
 }
 
 func Test_WorkerSetTask_Success(t *testing.T) {
+	suite.Run(t, new(WorkerSuite))
+
 	worker := NewWorker()
 	task := NewTask("job", time.Second, time.Second, 1, jobSimple)
 
