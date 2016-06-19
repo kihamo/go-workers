dbe69bfb9b9dcda5532ec8ec5a403e9f9a4e9084
Sergey Fedosenko <sergey.fedosenko@lazada.com>
2016-06-19 18:29:43 +0300

    Fix cover subpackages

--- .generate_coverage.sh
+++ .generate_coverage.sh
@@ -10,7 +10,7 @@ i=0
 for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_test.go' -type d);
 do
 if ls $dir/*.go &> /dev/null; then
-    go test -v -covermode=atomic -coverprofile=./cov/$i.out ./$dir
+    go test -v -coverprofile=./cov/$i.out ./$dir
     i=$((i+1))
 fi
 done
